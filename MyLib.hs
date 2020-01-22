9053a9a04ab4a9105a4f9c84d87a64a3040b19d2
Jake Vossen <jake@vossen.dev>
2020-01-22 08:43:15 -0700

    nroot

--- MyLib.hs
+++ MyLib.hs
@@ -1,4 +1,8 @@
 module MyLib (someFunc) where
 
+-- https://stackoverflow.com/questions/42708132/haskell-find-the-nth-root-of-a-float
+nroot :: (Integral a, Floating b) => a -> b -> b
+n `nroot` x = x ** (1 / fromIntegral n)
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
